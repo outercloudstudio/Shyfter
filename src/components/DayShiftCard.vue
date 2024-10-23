9759d124368f0aeebd061035ae22e2539cb0a953
Outer Cloud <outercloudstudio@gmail.com>
2024-10-22 23:21:08 -0500

    Fix conflicts

---
+++ src/components/DayShiftCard.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+<template>
+<Card title="Monday" icon="calendar_today">
+	<p>{{ shift.time }} {{ new Date((shift.day?.seconds ?? 0) * 1000) }}</p>
+</Card>
+</template>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
+import DayShiftCard from '@/components/DayShiftCard.vue'
 import NavigationMenu from '@/components/NavigationMenu.vue'
 
+import { Trade } from '@/libs/Firebase'
 import { shifts } from '@/libs/State';
+import { calculateTrades } from '@/libs/Trades'
+import { computed, ComputedRef } from 'vue'
+
+const trades: ComputedRef<Trade[]> = computed(() => calculateTrades(shifts.value))
 </script>
 
 <template>
 	<NavigationMenu />
 
 	<h1>Dashboard</h1>
-	<h3>Mini Calendar</h3>
-	<div v-for="shift in shifts">
-		<p>{{ shift.time }} {{ new Date((shift.day?.seconds ?? 0) * 1000) }}</p>
-	</div>
+	<h4>Mini Calendar</h4>
+	<DayShiftCard :day="new Date('October 23, 2024')" />
 
 	<h4>Available Trades</h4>
 	<div v-for="trade in trades">
 		<p>{{ trade.shift }}</p>
 		<p>{{ trade.from }}</p>
 	</div>
+
 	<!-- <h4>Requested Trades</h4>
 	<h4>Approved Trades</h4> -->
 </template>
