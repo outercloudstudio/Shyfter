db4823234b709c73b2aebf9ca952e200793050ca
Outer Cloud <outercloudstudio@gmail.com>
2024-10-22 22:25:39 -0500

    Login styling

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -19,13 +19,15 @@ async function onLogin() {
 </script>
 
 <template>
-	<div class="w-full h-full flex justify-center items-center">
+	<div class="w-full h-full flex justify-center items-center flex-col">
+		<h1 class="bold mb-8">Login</h1>
+
 		<Card title="Login" icon="deployed_code" :bold="true" >
 			<p>Email</p>
-			<input v-model="email" class="mb-2"/>
+			<input v-model="email" class="mb-2" placeholder="Email..."/>
 
 			<p>Password</p>
-			<input v-model="password" class="mb-4"/>
+			<input v-model="password" class="mb-4" placeholder="Pasword..." type="password"/>
 			
 			<div class="flex items-center justify-between">
 				<router-link to="register" class="link">Need An Account?</router-link>
